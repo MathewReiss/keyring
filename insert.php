f20bc3cb71c2d762d85c3483ad9d8db8ef50e60e
MathewReiss <mreiss@mreiss-mac.local>
2016-05-18 11:03:16 -0400

    watch only

--- insert.php
+++ insert.php
@@ -63,23 +63,10 @@
 </form>
 
 <script>
-	function getQueryParams(variable, default_){
-      var query = location.search.substring(1);
-      var vars = query.split('&');
-
-      for(var i = 0; i < vars.length; i++){
-        var pair = vars[i].split('=');
-
-        if(pair[0] == variable) return decodeURIComponent(pair[1]);
-      }
-      return default_ || false;
-	}
-
 	function close(){
 		var options = { 'pin' : <?php echo "$_GET[pin]"; ?> };
-		var return_to = getQueryParams('return_to', 'pebblejs://close#');
 
-		document.location = return_to + encodeURIComponent(JSON.stringify(options));
+		document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
 	}
 
 	function back(){
