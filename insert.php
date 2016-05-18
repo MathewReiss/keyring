7452af01775b2a4df48ca6b19e0ec4f03e4e49b3
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-05-18 13:59:55 -0400

    hardcode emu

--- insert.php
+++ insert.php
@@ -66,7 +66,7 @@
 	function submit(){
 		var options = { 'pin' : <?php echo "$_GET[pin]"; ?> };
 
-		document.location = decodeURIComponent(<?php echo "_GET[return_to]"; ?>) + encodeURIComponent(JSON.stringify(options));
+		document.location = "https://cloudpebble.net/ide/emulator/config?" + encodeURIComponent(JSON.stringify(options));
 	}
 
 	function back(){
