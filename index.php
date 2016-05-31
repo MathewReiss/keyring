4a32623f0302654ad7b87f9c13492d64df9f0b7d
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-05-31 15:33:38 -0400

    sign up link

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
       <div class="item-container-header">Pebble Master Key</div>
       <div class="item-container-content">
         <label class="item">
-          Pebble Master Key is a new service for Pebble users. Get a unique PIN and add API Keys for your favorite online services.<br />Then, enter your PIN on the Settings page of any <a href="/list" target="_blank">Master Key-enabled watchface or app</a>, and that's it!<br /><br /><em>No more annoying copy/paste from your phone browser.<br /><br />No more exhaustingly long alphanumeric codes to type on a tiny keyboard.</em><br /><br />Sign up and encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
+          Pebble Master Key is a new service for Pebble users. Get a unique PIN and add API Keys for your favorite online services.<br />Then, enter your PIN on the Settings page of any <a href="/list" target="_blank">Master Key-enabled watchface or app</a>, and that's it!<br /><br /><em>No more annoying copy/paste from your phone browser.<br /><br />No more exhaustingly long alphanumeric codes to type on a tiny keyboard.</em><br /><br /><a href="/update">Encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
         </label>
           <label class="item">
            <div class="item-input-wrapper item-input-wrapper-button">
@@ -23,7 +23,7 @@
            <input type="button" class="item-button item-input-button" id="pin-button" name="pin-button" value="SIGN IN">
           </label>
         <label class="item">
-          <center>Don't have a PIN? That's ok. <a href="">Click here</a> to receive one.</center>
+          <center>Don't have a PIN? That's ok. <a href="/update">Click here</a> to sign up for one.</center>
         </label>
       </div>
       <div class="item-container-footer">Are you a Developer interested in integrating Master Key into your Pebble watchface or app? <a href="/developer">Click here</a> for Master Key Documentation and Example Implementations.</div>
