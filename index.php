8f38b38e0cabb99cc01bcf6d320136701b62dca4
MathewReiss <mreiss@mreiss-mac.local>
2016-05-31 10:16:41 -0400

    spelling/format

--- index.php
+++ index.php
@@ -13,14 +13,15 @@
       <div class="item-container-header">Pebble Master Key</div>
       <div class="item-container-content">
         <label class="item">
-          Pebble Master Key is a new serive for Pebble users. Get a unique PIN and add API Keys for your favorite online services. Then, enter your PIN the Settings page of any <a href="" target="_blank">Master Key-enabled watchface or app</a>, and that's it! No more annoying copy/paste from your phone browser. No more exhaustingly long alphanumeric codes to enter on a tiny keyboard. Encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
+          Pebble Master Key is a new service for Pebble users. Get a unique PIN and add API Keys for your favorite online services. Then, enter your PIN the Settings page of any <a href="" target="_blank">Master Key-enabled watchface or app</a>, and that's it! No more annoying copy/paste from your phone browser. No more exhaustingly long alphanumeric codes to enter on a tiny keyboard. Encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
+        </label>
           <label class="item">
            <div class="item-input-wrapper item-input-wrapper-button">
              <input type="number" class="item-input" id="pin" name="pin" placeholder="5-Digit PIN">
            </div>
            <input type="button" class="item-button item-input-button" id="pin-button" name="pin-button" value="SIGN IN">
           </label>
-        </label>
+        
       </div>
     </div>
 </body>
