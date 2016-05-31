35a8a62efc18e8ca5635a8c9dea9d0a1dab7cda5
MathewReiss <mreiss@mreiss-mac.local>
2016-05-31 10:47:29 -0400

    favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   
   <title>Pebble Master Key</title>
   <link rel="stylesheet" href="https://dl.dropboxusercontent.com/s/9vzn6jmlvzogvi1/slate.css">
+  <link rel="icon" type="image/png" href="/favicon.png">
   <script type="text/javascript" src="https://dl.dropboxusercontent.com/s/zyba8qboh5czip8/slate.js"></script>
 </head>
 <body>
@@ -13,7 +14,7 @@
       <div class="item-container-header">Pebble Master Key</div>
       <div class="item-container-content">
         <label class="item">
-          Pebble Master Key is a new service for Pebble users. Get a unique PIN and add API Keys for your favorite online services.<br />Then, enter your PIN the Settings page of any <a href="" target="_blank">Master Key-enabled watchface or app</a>, and that's it!<br /><br /><em>No more annoying copy/paste from your phone browser.<br /><br />No more exhaustingly long alphanumeric codes to enter on a tiny keyboard.</em><br /><br />Encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
+          Pebble Master Key is a new service for Pebble users. Get a unique PIN and add API Keys for your favorite online services.<br />Then, enter your PIN on the Settings page of any <a href="" target="_blank">Master Key-enabled watchface or app</a>, and that's it!<br /><br /><em>No more annoying copy/paste from your phone browser.<br /><br />No more exhaustingly long alphanumeric codes to type on a tiny keyboard.</em><br /><br />Sign up and encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
         </label>
           <label class="item">
            <div class="item-input-wrapper item-input-wrapper-button">
