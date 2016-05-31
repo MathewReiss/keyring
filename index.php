7179fc8039b89d8f76971f9580bc912ed696c65f
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-05-31 15:29:03 -0400

    host slate locally

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0" name="viewport" />
   
   <title>Pebble Master Key</title>
-  <link rel="stylesheet" href="https://raw.githubusercontent.com/pebble/slate/master/dist/css/slate.min.css">
+  <link rel="stylesheet" href="/dist/css/slate.min.css">
   <link rel="icon" type="image/png" href="/favicon2.png">
-  <script type="text/javascript" src="https://raw.githubusercontent.com/pebble/slate/master/dist/js/slate.min.js"></script>
+  <script type="text/javascript" src="/dist/js/slate.min.js"></script>
 </head>
 <body>
   <div class="item-container">
