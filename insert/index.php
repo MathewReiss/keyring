ec2a0fb6f83bb9fd59d0fbd7a505d1ca22de319e
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-06-01 15:56:21 -0400

    no content type test

--- insert/index.php
+++ insert/index.php
@@ -1,8 +1,19 @@
 <?php
+    // respond to preflights
+    //if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // return only the headers and not the content
+    // only allow CORS if we're doing a GET - i.e. no saving for now.
+    //if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST') {
+    //    header('Access-Control-Allow-Origin: https://pmkey.xyz');
+    //    header('Access-Control-Allow-Headers: Content-Type');
+    //}
+    //exit;
+    //}
+
 	header("Access-Control-Allow-Headers: Content-Type");
 	header("Access-Control-Allow-Origin: https://pmkey.xyz");
 	header("Access-Control-Allow-Methods: POST");
-	header("Content-Type: application/json");
+	//header("Content-Type: application/json");
 
 	$servername = "$_ENV[DB_SERVERNAME]";
     $username = "$_ENV[DB_USER]";
