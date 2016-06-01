cd52914c70d2279945f9e176ff9c32ca00204378
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-06-01 14:27:20 -0400

    jk regex is always the answer

--- insert/index.php
+++ insert/index.php
@@ -1,7 +1,7 @@
 <?php
 
 	header("Access-Control-Allow-Headers: Content-Type");
-	header("Access-Control-Allow-Origin: *pmkey.xyz");
+	header("Access-Control-Allow-Origin: ^(https?://(?:.+\.)?pmkey\.xyz(?::\d{1,5})?)$");
 	header("Access-Control-Allow-Methods: POST");
 	header("Content-Type: application/json");
 	echo json_encode(array(success => true));
