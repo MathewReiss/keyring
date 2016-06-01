81fe1e3981f26cdf3a78d554d1c5df55e3d3412b
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-06-01 15:58:48 -0400

    cors

--- insert/index.php
+++ insert/index.php
@@ -2,7 +2,7 @@
     // respond to preflights
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     // return only the headers and not the content
-    // only allow CORS if we're doing a GET - i.e. no saving for now.
+    // only allow CORS if we're doing a POST - i.e. no saving for now.
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST') {
         header('Access-Control-Allow-Origin: https://pmkey.xyz');
         header('Access-Control-Allow-Headers: Content-Type');
@@ -10,9 +10,9 @@
     exit;
     }
 
-	//header("Access-Control-Allow-Headers: Content-Type");
-	//header("Access-Control-Allow-Origin: https://pmkey.xyz");
-	//header("Access-Control-Allow-Methods: POST");
+	header("Access-Control-Allow-Headers: Content-Type");
+	header("Access-Control-Allow-Origin: https://pmkey.xyz");
+	header("Access-Control-Allow-Methods: POST");
 	//header("Content-Type: application/json");
 
 	$servername = "$_ENV[DB_SERVERNAME]";
