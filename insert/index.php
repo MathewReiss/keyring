e71194b6c3a6621b185cbf478df92c453dce01dd
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-06-01 14:17:28 -0400

    CORS bullcrap

--- insert/index.php
+++ insert/index.php
@@ -1,5 +1,8 @@
 <?php
 
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: *");
+	header("Content-type: application/json");
 	echo json_encode(array(success => true));
 
 	/*
