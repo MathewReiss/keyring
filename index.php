efa77bccf881a7c6d37395092e4e1e9bfd24633c
MathewReiss <mreiss@mreiss-mac.local>
2016-06-02 07:36:02 -0400

    tweaks

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="item-container-header">Pebble Master Key</div>
     <div class="item-container-content">
       <label class="item">
-        Pebble Master Key is a new service for Pebble users. Get a unique PIN and add API Keys for your favorite online services.<br />Then, enter your PIN on the Settings page of any <a href="https://apps.getpebble.com/en_US/search/watchapps/1?native=false&query=pmkey.xyz" target="_blank">Master Key-enabled watchface or app</a>, and that's it!<br /><br /><em>No more annoying copy/paste from your phone browser.<br /><br />No more exhaustingly long alphanumeric codes to type on a tiny keyboard.</em><br /><br />Encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
+        Pebble Master Key is a new service for Pebble users. Get a unique PIN and add API Keys for your favorite online services.<br /><br />Then, enter your PIN on the Settings page of any <a href="https://apps.getpebble.com/en_US/search/watchapps/1?native=false&query=pmkey.xyz" target="_blank">Master Key-enabled watchface or app</a>, and that's it!<br /><br /><em>No more annoying copy/paste from your phone browser.<br /><br />No more long alphanumeric codes to type on a tiny keyboard.</em><br /><br />Encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
       </label>
       <label class="item">
         <div class="item-input-wrapper item-input-wrapper-button">
@@ -24,6 +24,9 @@
       </label>
       <label class="item">
         <center>Don't have a PIN? <a href="/update">Click here</a> to sign up for one.</center>
+      </label>
+      <label class="item">
+        <center><a href="mailto:mydogsnowy.pebble@gmail.com?subject=Master Key Support" target="_blank">Contact Support</a>.</center>
       </label>
     </div>
     <div class="item-container-footer">Are you a Developer interested in integrating Master Key into your Pebble watchface or app? <a href="/developer">Click here</a> for Master Key Documentation and Example Implementations.</div>
--- update/index.php
+++ update/index.php
@@ -165,6 +165,7 @@
       </div>
     </div>
 
+<!--
     <div class="item-container">
       <div class="item-container-header">Travel/Departure (First Class)</div>
       <div class="item-container-content">
@@ -176,6 +177,7 @@
         </label>
       </div>
     </div>
+-->
 
     <hr/>
     <br/>
@@ -192,7 +194,7 @@
     function saveKeys(){
       var xhr = new XMLHttpRequest();
       var url = "https://www.pmkey.xyz/insert/index.php";
-      url += "?pin=<?php echo $pin?>&id=<?php echo $id?>&date=<?php echo $date?>&owm=" + document.getElementById("owm").value + "&wu=" + document.getElementById("wu").value + "&forecast=" + document.getElementById("forecast").value + "&ifttt=" + document.getElementById("ifttt").value + "&wolfram=" + document.getElementById("wolfram").value + "&habits=" + document.getElementById("habits").value + "&travel=" + document.getElementById("travel").value;
+      url += "?pin=<?php echo $pin?>&id=<?php echo $id?>&date=<?php echo $date?>&owm=" + document.getElementById("owm").value + "&wu=" + document.getElementById("wu").value + "&forecast=" + document.getElementById("forecast").value + "&ifttt=" + document.getElementById("ifttt").value + "&wolfram=" + document.getElementById("wolfram").value + "&habits=" + document.getElementById("habits").value;// + "&travel=" + document.getElementById("travel").value;
       xhr.open("GET", url, true);
 
       document.getElementById("save-button").value = "SAVING...";
