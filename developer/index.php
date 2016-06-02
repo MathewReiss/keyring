f0af8ab8696f81958334365b153aded1f20112ff
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-06-02 13:21:12 -0400

    dev refresh

--- developer/index.php
+++ developer/index.php
@@ -14,28 +14,9 @@
       <div class="item-container-header">Pebble Master Key</div>
       <div class="item-container-content">
         <label class="item">
-          Pebble Master Key is a new service for Pebble users. Get a unique PIN and add API Keys for your favorite online services.<br />Then, enter your PIN on the Settings page of any <a href="/list" target="_blank">Master Key-enabled watchface or app</a>, and that's it!<br /><br /><em>No more annoying copy/paste from your phone browser.<br /><br />No more exhaustingly long alphanumeric codes to type on a tiny keyboard.</em><br /><br />Sign up and encourage the developers of your favorite Pebble faces and apps to start using Master Key, today!
+            
         </label>
-          <label class="item">
-           <div class="item-input-wrapper item-input-wrapper-button">
-             <input type="number" class="item-input" id="pin" name="pin" placeholder="5-Digit PIN">
-           </div>
-           <input type="button" class="item-button item-input-button" id="pin-button" name="pin-button" value="SIGN IN">
-          </label>
-        <label class="item">
-          <center>Don't have a PIN? That's ok. <a href="">Click here</a> to receive one.</center>
-        </label>
-      </div>
-      <div class="item-container-footer">Are you a Developer interested in integrating Master Key into your Pebble watchface or app? <a href="/developer">Click here</a> for Master Key Documentation and Example Implementations.</div>
-    </div>
-
-    <div class="item-container">
-      <div class="item-container-footer">
-        <label class="item">
-          <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.pmkey.xyz" data-text="Check out #MasterKey for @Pebble! Makes life so much easier." data-size="large">Tweet #MasterKey</a>
-          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-        </label>
-      </div>
+      <div class="item-container-footer">Master Key is a 3rd Party Service not affiliated with Pebble Technology Corp. All references to Pebble™ are purely for descriptive purposes.</div>
     </div>
 </body>
 </html>
