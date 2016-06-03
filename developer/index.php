1cc1d97ae0f2bcf121825bc3de111753da2fb6dd
MathewReiss <mreiss@mreiss-mac.wifi.tripadvisor.com>
2016-06-03 11:25:19 -0400

    update docs

--- developer/index.php
+++ developer/index.php
@@ -14,7 +14,7 @@
       <div class="item-container-header">Pebble Master Key</div>
       <div class="item-container-content">
         <label class="item">
-            Master Key makes it easier than ever to ask your users to provide their own API keys for services with strict rate-limits. Rather than asking for a long Weather Underground API Key, for example, all you need from your user is their 5-digit Master Key PIN.
+            Master Key makes it easier than ever to ask your users to provide their own API keys for services with strict rate-limits. Rather than asking for a long Weather Underground API Key, for example, all you need from your user is their email address and 5-digit Master Key PIN.
         </label>
         <label class="item">
           <strong><font size="+2">Requirements</font></strong><br />
@@ -24,7 +24,7 @@
         </label>
         <label class="item">
             <strong><font size="+2">API Endpoint</font></strong><br />
-            To get the relevant API keys, simply call the following URL via HTTP GET, passing the user's PIN as a parameter: <pre>https://pmkey.xyz/search/?pin=10000</pre><br />
+            To get the relevant API keys, simply call the following URL via HTTP GET, passing the user's email and PIN as parameters: <pre>https://pmkey.xyz/search/?email=testuser@email.com&amp;pin=10000</pre><br />
             The JSON response will look like the following:<br />
             <pre>
   {
@@ -65,14 +65,14 @@
     var config = JSON.parse(decodeURIComponent(e.response));
 
     var xhr = new XMLHttpRequest();
-    var url = "https://pmkey.xyz/search/?pin=" + config.MasterKeyPIN;
+    var url = "https://pmkey.xyz/search/?email=" + config.Email + "&amp;pin=" + config.MasterKeyPIN;
   
     xhr.open("GET", url, true);
   
     xhr.onreadystatechanged = function(){
-      if(xhr.readyState == 4 && xhr.status == 200){
+      if(xhr.readyState == 4 &amp;&amp; xhr.status == 200){
         var result = JSON.parse(xhr.responseText);
-        if(result.success && result.keys.weather.wu !== ""){
+        if(result.success &amp;&amp; result.keys.weather.wu !== ""){
           localStorage.setItem("wuKey", result.keys.weather.wu);
         }
       }
@@ -96,10 +96,13 @@
       &lt;div class="item-container"&gt;
         &lt;div class="item-container-content"&gt;
           &lt;div class="item"&gt;
-            Master Key allows your favorite Pebble watchfaces and apps to grab all your API keys via a simple 5-digit PIN code.&lt;br /&gt;
-            To sign up, visit the &lt;a href="http://www.pmkey.xyz" target="_blank"&gt;Pebble Master Key Website&lt;/a&gt;.
+            Master Key allows your favorite Pebble watchfaces and apps to grab all your API keys via email and a simple 5-digit PIN code.&lt;br /&gt;
+            To sign up, visit the Pebble Master Key Website (pmkey.xyz).
           &lt;/div&gt;
           &lt;label class="item"&gt;
+            &lt;div class="item-input-wrapper item-input-wrapper-button"&gt;
+              &lt;input type="text" class="item-input" id="email"&gt;
+            &lt;/div&gt;
             &lt;div class="item-input-wrapper item-input-wrapper-button"&gt;
               &lt;input type="number" class="item-input" id="master"&gt;
             &lt;/div&gt;
@@ -116,7 +119,7 @@
       &lt;script&gt;
         function sync(){
           var xhr = new XMLHttpRequest();
-          var url = "https://pmkey.xyz/search/?pin=" + document.getElementById("master").value;
+          var url = "https://pmkey.xyz/search/?email=" document.getElementById("email").value + "&amp;pin=" + document.getElementById("master").value;
 
           xhr.open("GET", url, true);
 
@@ -124,7 +127,7 @@
           masterSync.value = "LOADING...";
 
           xhr.onreadystatechange = function(){
-            if(xhr.readyState == 4 && xhr.status == 200){
+            if(xhr.readyState == 4 &amp;&amp; xhr.status == 200){
               if(result.success){
                 masterSync.value = "SUCCESS";
                 document.getElementById("wu").value = result.keys.weather.wu;
