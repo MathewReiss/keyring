00045a873545ed56e4a28724a2c609a80755b317
MathewReiss <mreiss@mreiss-mac.local>
2016-06-06 07:00:12 -0400

    subtraction

--- search/index.php
+++ search/index.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
-    if(isset($_SESSION('LAST_CALL'))){
+    if(isset($_SESSION('LAST_CALL'))) {
         $last = strtotime($_SESSION('LAST_CALL'));
         $curr = strtotime(date("Y-m-d h:i:s"));
-        $sec = abs($last = $curr);
+        $sec = abs($last - $curr);
         if(sec <= 30){
         	header("Content-Type: application/json");
             echo json_encode(array(success => false, error => "Rate limit exceeded"));
