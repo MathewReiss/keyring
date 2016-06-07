bfd113ea1e861d2d7ae7906d3585757fe577a93e
MathewReiss <mreiss@mreiss-mac.local>
2016-06-07 07:37:13 -0400

    real daily limit

--- search/index.php
+++ search/index.php
@@ -15,7 +15,7 @@
         }
     }
     if(isset($_SESSION['NUM_CALLS'])){
-    	if($_SESSION['NUM_CALLS'] >= 10){
+    	if($_SESSION['NUM_CALLS'] >= 100){
     		header("Access-Control-Allow-Headers: Content-Type");
 			header("Access-Control-Allow-Origin: *");
 			header("Access-Control-Allow-Methods: GET");
