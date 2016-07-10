58260deb58ba021aa99c57f3fcc68335c8e01f1c
MathewReiss <mreiss@mreiss-mac.local>
2016-07-10 13:53:26 -0400

    travel branding

--- update/index.php
+++ update/index.php
@@ -179,10 +179,10 @@
     </div>
 
     <div class="item-container">
-      <div class="item-container-header">Priority Access</div>
+      <div class="item-container-header">Travel Priority Access</div>
       <div class="item-container-content">
         <label class="item">
-          To get an API Key for Priority Access, open the Travel Settings page in your Pebble Time app (First-Class users only).</a>.
+          To get your Travel ID for Priority Access, open the Travel Settings page in your Pebble Time app (First-Class users only).
           <div class="item-input-wrapper">
             <input type="text" class="item-input" id="travel" name="travel" value="<?php echo $travel?>">
           </div>
