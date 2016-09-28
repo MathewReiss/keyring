bb4bd90cbf305a11259700592f07534ae31a4674
MathewReiss <mathew_reiss@alumni.brown.edu>
2016-09-28 17:21:43 -0400

    Simple email

--- update/index.php
+++ update/index.php
@@ -57,7 +57,50 @@
               $pin = rand(10000,99999);//bindec(strrev(str_pad(decbin($id), 16, '0', STR_PAD_LEFT))) + 10000;
 
               $sql = "UPDATE $tablename SET pin='$pin' WHERE id=$id;";
-              $mysqli->query($sql);  
+              $mysqli->query($sql);
+		    
+		  //BEGIN EMAIL CODE 
+		    
+	      define('POSTMARK_API_KEY', "$_ENV[POSTMARK_API_KEY]");
+function send_email($email, &$response = null, &$http_code = null) {
+    $json = json_encode(array(
+        'From' => $email['from'],
+        'To' => $email['to'],
+        'Cc' => $email['cc'],
+        'Bcc' => $email['bcc'],
+        'Subject' => $email['subject'],
+        'Tag' => $email['tag'],
+        'HtmlBody' => $email['html_body'],
+        'TextBody' => $email['text_body'],
+        'ReplyTo' => $email['reply_to'],
+        'Headers' => $email['headers'],
+        'Attachments' => $email['attachments']
+    ));
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://api.postmarkapp.com/email');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+        'X-Postmark-Server-Token: ' . POSTMARK_API_KEY
+    ));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    $response = json_decode(curl_exec($ch), true);
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    return $http_code === 200;
+}
+
+$sent = send_email(array(
+    'to' => $email,
+    'from' => 'Master Key <donotreply@pmkey.xyz>',
+    'subject' => 'Your Master Key PIN',
+    'text_body' => 'Your email is ' . $email . ' and your PIN is ' . $pin . '.',
+    'html_body' => '<html><body>Your email is <em>' . $email . '</em> and your PIN is <strong>' . $pin . '</strong>.<br /><br />Be sure to check out all of the awesome <a href="https://apps.getpebble.com/en_US/collection/master-key/watchfaces/1" target="_blank">Master Key-enabled watchfaces in the Pebble Appstore</a>.</body></html>'
+), $response, $http_code);
+		    
+		    //END EMAIL CODE
               
 	      echo '</font></div></div><script>document.location = document.location + "&pin=' . $pin . '"; localStorage.setItem("savedPin", "' . $pin . '"); </script>';
             }
